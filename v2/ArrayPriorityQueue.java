80022f8c8e3a5dd0d510268cedc1172d8f984873
Calvin Aw <33496390+caw024@users.noreply.github.com>
2018-05-14 08:19:39 -0400

    Update ArrayPriorityQueue.java

--- v2/ArrayPriorityQueue.java
+++ v2/ArrayPriorityQueue.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
 
+//start of class
 public class ArrayPriorityQueue implements PriorityQueue{
+	
+	//instance variables
     private ArrayList<String> ArrPQ;
     private String Al_data;
 
+	//constructors
     public ArrayPriorityQueue(){
 	ArrPQ = new ArrayList<String>();
     }
 
+	//O(n): Adds string to Arraylist based on its value, uses Arraylist implementation
     public void add(String x){
 	for (int i = 0; i < ArrPQ.size(); i++){
 	    Al_data = ArrPQ.get(i);
@@ -18,16 +23,23 @@ public class ArrayPriorityQueue implements PriorityQueue{
 	}
 	ArrPQ.add(x);
     }
+	//O(1): returns whether the arraylist is empty
     public boolean isEmpty(){
 	return ArrPQ.size() == 0;
     }
+	
+	//O(1): returns the minimum value of the arraylist
     public String peekMin(){
 	return ArrPQ.get(0);
     }
+	
+	//O(n): removes the minimum value of the arraylist
+	//Uses ArrayList implementation of remove()
     public String removeMin(){
 	return ArrPQ.remove(0);
     }
 
+	//main method
     public static void main(String[] args){
 	ArrayPriorityQueue fun = new ArrayPriorityQueue();
 	System.out.println("Is fun empty? " + fun.isEmpty());
